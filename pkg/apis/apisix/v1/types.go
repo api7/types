81f6d99a52073b0917134ab544961562e0a5435d
kv <gxthrj@163.com>
2020-01-22 10:04:52 +0800

    add resourceVersion for types

--- pkg/apis/apisix/v1/types.go
+++ pkg/apis/apisix/v1/types.go
@@ -5,16 +5,17 @@ import "encoding/json"
 // Route apisix route object
 // +k8s:deepcopy-gen=true
 type Route struct {
-	ID           *string   `json:"id,omitempty" yaml:"id,omitempty"`
-	Host         *string   `json:"host,omitempty" yaml:"host,omitempty"`
-	Path         *string   `json:"path,omitempty" yaml:"path,omitempty"`
-	Name         *string   `json:"name,omitempty" yaml:"name,omitempty"`
-	Methods      []*string `json:"methods,omitempty" yaml:"methods,omitempty"`
-	ServiceId    *string   `json:"service_id,omitempty" yaml:"service_id,omitempty"`
-	ServiceName  *string   `json:"service_name,omitempty" yaml:"service_name,omitempty"`
-	UpstreamId   *string   `json:"upstream_id,omitempty" yaml:"upstream_id,omitempty"`
-	UpstreamName *string   `json:"upstream_name,omitempty" yaml:"upstream_name,omitempty"`
-	Plugins      *Plugins  `json:"plugins,omitempty" yaml:"plugins,omitempty"`
+	ID              *string   `json:"id,omitempty" yaml:"id,omitempty"`
+	ResourceVersion *string   `json:"resource_version,omitempty" yaml:"resource_version,omitempty"`
+	Host            *string   `json:"host,omitempty" yaml:"host,omitempty"`
+	Path            *string   `json:"path,omitempty" yaml:"path,omitempty"`
+	Name            *string   `json:"name,omitempty" yaml:"name,omitempty"`
+	Methods         []*string `json:"methods,omitempty" yaml:"methods,omitempty"`
+	ServiceId       *string   `json:"service_id,omitempty" yaml:"service_id,omitempty"`
+	ServiceName     *string   `json:"service_name,omitempty" yaml:"service_name,omitempty"`
+	UpstreamId      *string   `json:"upstream_id,omitempty" yaml:"upstream_id,omitempty"`
+	UpstreamName    *string   `json:"upstream_name,omitempty" yaml:"upstream_name,omitempty"`
+	Plugins         *Plugins  `json:"plugins,omitempty" yaml:"plugins,omitempty"`
 }
 
 type Plugins map[string]interface{}
@@ -36,21 +37,23 @@ func (p *Plugins) DeepCopy() *Plugins {
 // Service apisix service
 // +k8s:deepcopy-gen=true
 type Service struct {
-	ID           *string  `json:"id,omitempty" yaml:"id,omitempty"`
-	Name         *string  `json:"name,omitempty" yaml:"name,omitempty"`
-	UpstreamId   *string  `json:"upstream_id,omitempty" yaml:"upstream_id,omitempty"`
-	UpstreamName *string  `json:"upstream_name,omitempty" yaml:"upstream_name,omitempty"`
-	Plugins      *Plugins `json:"plugins,omitempty" yaml:"plugins,omitempty"`
+	ID              *string  `json:"id,omitempty" yaml:"id,omitempty"`
+	ResourceVersion *string  `json:"resource_version,omitempty" yaml:"resource_version,omitempty"`
+	Name            *string  `json:"name,omitempty" yaml:"name,omitempty"`
+	UpstreamId      *string  `json:"upstream_id,omitempty" yaml:"upstream_id,omitempty"`
+	UpstreamName    *string  `json:"upstream_name,omitempty" yaml:"upstream_name,omitempty"`
+	Plugins         *Plugins `json:"plugins,omitempty" yaml:"plugins,omitempty"`
 }
 
 // Upstream apisix upstream
 // +k8s:deepcopy-gen=true
 type Upstream struct {
-	ID    *string `json:"id,omitempty" yaml:"id,omitempty"`
-	Name  *string `json:"name,omitempty" yaml:"name,omitempty"`
-	Type  *string `json:"type,omitempty" yaml:"type,omitempty"`
-	Key   *string `json:"key,omitempty" yaml:"key,omitempty"`
-	Nodes []*Node `json:"nodes,omitempty" yaml:"nodes,omitempty"`
+	ID              *string `json:"id,omitempty" yaml:"id,omitempty"`
+	ResourceVersion *string `json:"resource_version,omitempty" yaml:"resource_version,omitempty"`
+	Name            *string `json:"name,omitempty" yaml:"name,omitempty"`
+	Type            *string `json:"type,omitempty" yaml:"type,omitempty"`
+	Key             *string `json:"key,omitempty" yaml:"key,omitempty"`
+	Nodes           []*Node `json:"nodes,omitempty" yaml:"nodes,omitempty"`
 }
 
 // Node the node in upstream
